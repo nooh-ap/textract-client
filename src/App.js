9c65f567aec06b899e07db7f20dfea0c3aeae55a
Nooh AlMahfoodhi <nooh.almahfoodhi@gmail.com>
2023-06-24 04:33:26 -0700

    Added Tailwind, Removed MUI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import UploadImage from "./UploadImage/UploadImage";
+import Header from "../src/Components/Header";
+import Footer from "../src/Components/Footer";
+
 function App() {
   const onClick = () => {
     console.log("clicked");
   };
+
   return (
-    <div className="App">
-      <h1>Track Reciept</h1>
-      <div>
-        <p>
-          <label for="imageFile">Upload a photo of yourself:</label>
-          <input type="file" id="imageFile" capture="environment" accept="image/*" />
-        </p>
+    <div className="grid grid-rows-3 h-96 text-center">
+      <Header className="bg-teal-300" />
+      <div className="flex-col  ">
+        <UploadImage />
+      </div>
+
+      <div className="fixed bottom-0">
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-gray
+         text-black">
+            <p className='w-96 h-8'>© 2021, All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer;
