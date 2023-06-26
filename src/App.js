e8e3d0ab7e618ea9a12988c56547bef9dd64d241
Nooh AlMahfoodhi <nooh.almahfoodhi@gmail.com>
2023-06-26 02:46:56 -0700

    Added AWS Amplify

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import "./App.css";
 import UploadImage from "./UploadImage/UploadImage";
 import Header from "../src/Components/Header";
 import Footer from "../src/Components/Footer";
-
+import {
+  HeroLayout1 
+ } from './ui-components';
+ 
 function App() {
   const onClick = () => {
     console.log("clicked");
   };
 
-  return (
-    <div className="grid grid-rows-3 h-96 text-center">
-      <Header className="bg-teal-300" />
-      <div className="flex-col  ">
-        <UploadImage />
-      </div>
-
-      <div className="fixed bottom-0">
-        <Footer />
-      </div>
-    </div>
+  return (<div>
+    <HeroLayout1 />
+  </div>
   );
 }
 
 export default App;
+
+{/* <div className="grid grid-rows-3 h-96 text-center">
+<Header className="" />
+<div className="flex-col  ">
+  <UploadImage />
+</div>
+
+<div className="fixed bottom-0">
+  <Footer />
+</div>
+</div> */}
