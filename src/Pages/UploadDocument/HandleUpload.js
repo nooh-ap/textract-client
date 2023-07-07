8a50fba878323ac50dcb7ccb3677f73aa6e4e2a6
Nooh AlMahfoodhi <nooh.almahfoodhi@gmail.com>
2023-07-06 18:16:57 -0700

    feat: Add GET method to upload page

--- src/Pages/UploadDocument/HandleUpload.js
+++ src/Pages/UploadDocument/HandleUpload.js
@@ -1,41 +1,50 @@
 import {useState} from "react";
 import {Button, Center} from "@chakra-ui/react";
 
-const headers = {
-    "Content-Type": "multipart/form-data",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-};
 
-const url = "https://d5ggcgl4u4.execute-api.us-east-1.amazonaws.com/test/textract/analyzereciept"
-const uploadFileToAWS = (file) => {
-    const formData = new FormData();
-    formData.append("image", file);
-    fetch(url, {
-        method: "POST",
-        mode: "cors",
-        headers: headers,
-        body: formData,
-    })
-        .then((response) => response.json())
-        .then((result) => {
-            console.log("Success:", result);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-};
 
 
 const HandleUpload = () => {
     const [file, setFile] = useState(null);
 
-    const uploadFile = (selectedFile) => {
-        setFile(selectedFile);
-        uploadFileToAWS(selectedFile)
-    }
+    // const uploadFile =  (selectedFile) => {
+    //     console.log(selectedFile, "Uploaded file");
+    //
+    //     const formdata = new FormData();
+    //     formdata.append("file", selectedFile, "react-image.jpeg");
+    //
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         body: formdata,
+    //         headers: {
+    //             "Content-Type": "multipart/form-data",
+    //             "Access-Control-Allow-Origin": "http://localhost:3000/",
+    //             "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+    //         },
+    //         redirect: 'follow'
+    //     };
+    //
+    //     fetch("https://d5ggcgl4u4.execute-api.us-east-1.amazonaws.com/test/textract/analyzeReceipt", requestOptions)
+    //         .then(response => response.json())
+    //         .then(result => console.log(result))
+    //         .catch(error => console.log('error', error));
+    //
+    //     console.log("Scanned file");
+    // }
+
+
     const handleClick = () => {
-        document.getElementById("imageFile").click();
+        // document.getElementById("imageFile").click();
+
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://d5ggcgl4u4.execute-api.us-east-1.amazonaws.com/test/textract", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     };
 
 
@@ -60,9 +69,9 @@ const HandleUpload = () => {
                     type="file"
                     id="imageFile"
                     accept="image/*"
-                    onChange={(event) => {
-                        uploadFile(event.target.files[0]);
-                    }}
+                    // onChange={(event) => {
+                    //     uploadFile(event.target.files[0]);
+                    // }}
                     hidden
                 />
             </div>
